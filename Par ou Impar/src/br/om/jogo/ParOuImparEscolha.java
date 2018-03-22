2dfc89ecff2a5854a4fe36a332e0bf05b7e6986d
giovane.fforlenza <giovane.fforlenza@TAT0541250W10-1.senacsp.edu.br>
2018-03-22 19:49:18 -0300

    Ajuste de erros de formatação Adicionei comentários explicando a lógica para a cração do projeto

--- Par ou Impar/src/br/om/jogo/ParOuImparEscolha.java
+++ Par ou Impar/src/br/om/jogo/ParOuImparEscolha.java
@@ -2,81 +2,86 @@ package br.om.jogo;
 
 import java.util.Random;
 import java.util.Scanner;
- // Atualização pelo GitHub
+
+// Atualização pelo GitHub
 public class ParOuImparEscolha {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		Random num = new Random();
-		int jogador=0, computador =0;
+		// Declara��o das vari�veis
+		int jogador = 0, computador = 0;
 		String escjog = "a", esccomp = "a";
+
 		do {
-			int i =1;
-		System.out.println("Esolha:");
-		System.out.println("[1] Par");
-		System.out.println("[2] Ímpar");
-		int jog = teclado.nextInt();
-		switch (jog) {
-		case 1:
-			escjog = "Par";
-			esccomp = "Ímpar";
-			break;
-		case 2:
-			escjog = "Par";
-			esccomp = "Ímpar";
-			break;
-		default:
-			System.out.println("Opção inválida");
-			break;
-		}
-		System.out.print("Digite um número: ");
-		int escjo = teclado.nextInt();
-		int esccom = num.nextInt(10);
-		int total = escjo + esccom ;
-		System.out.println();
-		System.out.println("Rodada " + i);
-		System.out.println("=============================");
-		System.out.println("Jogador: " + escjog);
-		System.out.println("Computador: " + esccomp);
-		System.out.println("=============================");
-		System.out.println(escjo + " + " + esccom + " = " + total);
-		System.out.println("=============================");
-		if(total%2==0) {
-			if(escjog=="Par" && esccomp=="Ímpar") {
-				System.out.println("Jogador ganhou a rodada!");
-				jogador++;
-			}else if(escjog=="Ímpar" && esccomp=="Par") {
-				System.out.println("Computador ganhou a rodada!");
-				computador++;
-			}else {
-				System.out.println("Empate");
+			int i = 1;
+			//Jogador escolhe Par/Impar
+			System.out.println("Esolha:");
+			System.out.println("[1] Par");
+			System.out.println("[2] Impar");
+			int jog = teclado.nextInt();
+			switch (jog) {
+			case 1:
+				//Caso jogador escolha Par, computador escolhe Impar
+				escjog = "Par";
+				esccomp = "Impar";
+				break;
+			case 2:
+				//Caso jogador escolha Impar, computador escolhe Par
+				escjog = "Impar";
+				esccomp = "Parr";
+				break;
+			default:
+				System.out.println("Opcao Invalida");
+				break;
 			}
-		}else if(total%2==1) {
-			if(escjog=="Par" && esccomp=="Ímpar") {
-				System.out.println("Computador ganhou a rodada!");
-				computador++;
-			}else if(escjog=="Ímpar" && esccomp=="Par") {
-				System.out.println("Jogador gahou a rodada!");
-				jogador++;
-			}else {
-				System.out.println("Empate");
+			//Jogador escolhe um numero
+			System.out.print("Digite um numero: ");
+			int escjo = teclado.nextInt();
+			int esccom = num.nextInt(10);
+			//Computador escolhe um numero entre 0 e 10
+			int total = escjo + esccom;
+			System.out.println();
+			System.out.println("Rodada " + i);
+			System.out.println("=============================");
+			System.out.println("Jogador: " + escjog);
+			System.out.println("Computador: " + esccomp);
+			System.out.println("=============================");
+			System.out.println(escjo + " + " + esccom + " = " + total);
+			System.out.println("=============================");
+			//Analise dos resultados
+			if (total % 2 == 0) {
+				if (escjog == "Par" && esccomp == "Impar") {
+					System.out.println("Jogador ganhou a rodada!");
+					jogador++;
+				} else if (escjog == "Impar" && esccomp == "Par") {
+					System.out.println("Computador ganhou a rodada!");
+					computador++;
+				} else {
+					System.out.println("Empate");
+				}
+			} else if (total % 2 == 1) {
+				if (escjog == "Par" && esccomp == "Impar") {
+					System.out.println("Computador ganhou a rodada!");
+					computador++;
+				} else if (escjog == "Impar" && esccomp == "Par") {
+					System.out.println("Jogador gahou a rodada!");
+					jogador++;
+				} else {
+					System.out.println("Empate");
+				}
 			}
-		}
-		System.out.println(jogador + " x " + computador);
-		System.out.println("=============================");
-		System.out.println();
-		i++;
-		if(jogador==3) {
-			System.out.println("Jogador ganhou o jogo!");
-			System.exit(0);
-		}else if(computador == 3) {
-			System.out.println("Computador ganhou o jogo!");
-			System.exit(0);
-		}
-			
-		}while(jogador<4);
+			//Mostra resultado
+			System.out.println(jogador + " x " + computador);
+			System.out.println("=============================");
+			System.out.println();
+			i++;
+			//Quando um dos 2 chega em 3 pontos, o programa acaba e informa quem ganhou o jogo
+			if (jogador == 3) {
+				System.out.println("Jogador ganhou o jogo!");
+			} else if (computador == 3) {
+				System.out.println("Computador ganhou o jogo!");
+			}
+
+		} while (jogador < 4 || computador < 4);
 	}
 }
-
-		
-		
-
